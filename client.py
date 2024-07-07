c87e4f5bcf49d0a6e8fc15a530e916a72637634d
unknown <jihadhijazi977@gmail.com>
2024-07-08 00:13:42 +0300

    added and teated yaml in python file:

--- client.py
+++ client.py
@@ -1,4 +1,10 @@
+import yaml
 # test comment frontend
 #testing auto sync=
 #testing auto sync 2
 #testing auto sync 3
+if __name__ == "__main__":
+    with open("../conf.yaml", 'r') as file:
+        config = yaml.safe_load(file)
+        print(f"Server IP Address: {config.get('ServerIPAddress')}")
+        
